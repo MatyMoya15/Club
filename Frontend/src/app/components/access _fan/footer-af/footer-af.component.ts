e071ac6595ef5abd8cb3a8a24adc12c126da6fb6
Maty Moyano <matymoyano4@gmail.com>
2026-02-04 14:40:06 -0300

    Footer access fan

---
+++ Frontend/src/app/components/access _fan/footer-af/footer-af.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-af',
+  templateUrl: './footer-af.component.html',
+  styleUrls: ['./footer-af.component.css']
+})
+export class FooterAfComponent {
+
+}
