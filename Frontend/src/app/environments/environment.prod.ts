3730752974ec4979bcc43668433b8bb70f5bf9f5
Maty Moyano <matymoyano4@gmail.com>
2026-02-22 23:02:02 -0300

    Pequeño cambio

---
+++ Frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://club-backend-8lb1.onrender.com/api',
+  emailjs: {
+    publicKey: 'l48pEU6XzzybpVLOC',
+    serviceId: 'service_nilwl4k',
+    templateId: 'template_ohsv4iy'
+  }
+};
