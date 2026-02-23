0c4316473dcd957ee88b7539f0ff8e02255ef73f
Maty Moyano <matymoyano4@gmail.com>
2026-02-22 23:06:52 -0300

    Too

---
+++ Frontend/src/app/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5000/api',
+  emailjs: {
+    publicKey: 'l48pEU6XzzybpVLOC',
+    serviceId: 'service_nilwl4k',
+    templateId: 'template_ohsv4iy'
+  }
+};
